cce8bcdb7c9ad31976e8492dcb77d449a51c1251
readf0x <davis.a.forsythe@gmail.com>
2024-12-24 15:42:57 -0600

    added nix packaging

--- flake.nix
+++ flake.nix
@@ -14,6 +14,24 @@
           GOPATH = "/home/readf0x/.config/go";
           packages = [ pkgs.go ];
         };
+        packages = rec {
+          islive = pkgs.buildGoModule {
+            name = "islive";
+            pname = "islive";
+
+            src = ./.;
+
+            vendorHash = null;
+
+            meta = {
+              description = "Check if a twitch streamer is live";
+              mainProgram = "islive";
+              homepage = "https://github.com/Readf0x/islive";
+              license = pkgs.lib.licenses.cc0;
+            };
+          };
+          default = islive;
+        };
       };
     };
 }
